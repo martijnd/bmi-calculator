09c093930b010bb3252bcfaf6ff7a3076b4709f7
martijnd <martijn.dorsman@gmail.com>
2019-08-11 22:58:20 +0200

    Setup tailwind and svelte

---
+++ tailwind.js
@@ -0,0 +1,29 @@
+module.exports = {
+  theme: {
+    fontSize: {
+      xs: ".75rem",
+      sm: ".875rem",
+      base: "1rem",
+      lg: "1.125rem",
+      xl: "1.25rem",
+      "2xl": "1.5rem",
+      "3xl": "1.875rem",
+      "4xl": "2.25rem",
+      "5xl": "3rem",
+      "6xl": "4rem",
+      "8xl": "6rem"
+    },
+    extend: {
+      colors: {
+        primary: "#5A44A2",
+        "primary-dark": "#34275E",
+        "theme-green": "#75AA6C",
+        "theme-orange": "#E1AD27",
+        "theme-red": "#D24444",
+        "light-text": "#B1A5D9"
+      }
+    }
+  },
+  variants: {},
+  plugins: []
+};
