41c5cf681cb392628f93f5da68d69141e68e224e
martijnd <martijn.dorsman@gmail.com>
2019-08-11 22:58:29 +0200

    Added app and card component

---
+++ src/App.svelte
@@ -0,0 +1,44 @@
+<script>
+  import Card from "./Card.svelte";
+  let weight;
+  let height;
+  let age;
+  let gender;
+  let bmi;
+
+  const updateBMI = event => {
+    const { title, data } = event.detail;
+    switch (title.toLowerCase()) {
+      case "gewicht":
+        weight = data;
+        break;
+      case "leeftijd":
+        age = data;
+        break;
+      case "lengte":
+        height = data;
+        break;
+      case "geslacht":
+        gender = data;
+      default:
+        console.error("wrong data type returned: ", event);
+        break;
+    }
+
+    if (weight && length && age) {
+      bmi = weight / height ** height;
+    }
+  };
+</script>
+
+<div class="w-screen min-h-screen bg-primary antialiased">
+  <div class="container mx-auto p-6">
+    <h1
+      class="text-yellow-500 text-center text-3xl uppercase font-bold"
+      style="line-height: 0.8;">
+      Bereken je
+      <span class="block text-8xl">BMI</span>
+    </h1>
+    <Card title="Gewicht" unit="kg" on:update={updateBMI} />
+  </div>
+</div>
---
+++ src/Card.svelte
@@ -0,0 +1,25 @@
+<script>
+  import { createEventDispatcher } from "svelte";
+
+  export let title;
+  export let unit;
+
+  const dispatch = createEventDispatcher();
+
+  const update = event => {
+    console.log(event);
+    dispatch("update", { title, data: event.data });
+  };
+</script>
+
+<div class="Card bg-primary-dark mt-4 max-w-xs mx-auto rounded-lg p-4">
+  <h2 class="text-center text-primary text-3xl font-bold uppercase">{title}</h2>
+  <input
+    class="border-none bg-transparent mx-auto w-full text-6xl text-white
+    text-center"
+    type="number"
+    on:input={update}
+    step="1"
+    min="1" />
+  <span class="absolute">{unit}</span>
+</div>
